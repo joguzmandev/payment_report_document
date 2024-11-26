3df36bd91e3cce6da51382fd7ab4a85101fa970b
Jorge Guzman <jguzman@argentuminc.com>
2024-11-26 18:01:51 -0400

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Reporte de Pagos personalizados",
+    "summary": "Reporte de Pagos personalizados",
+    "version": "1.0",
+    "depends": ["account"],
+    "author": "JGuzmanDev",
+    "category": "Accounting",
+    "data": ["views/payment_report_document.xml"],
+    "installable": True,
+    "auto_install": False,
+}
